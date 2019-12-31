e8c5cdfb32dcdc9c25632b30d33fcf5a31e4c19d
Coty <cotyhamilton@gmail.com>
2019-12-31 15:38:46 -0600

    Update Layout.js

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -79,7 +79,7 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
                 <footer className="site-foot">
                     <div className="site-foot-nav container">
                         <div className="site-foot-nav-left">
-                            <Link to="/">{site.title}</Link> © 2019 - 20 &mdash; You can stop reading now
+                            <Link to="/">{site.title}</Link> © 2019, 2020 &mdash; You can stop reading now
                         </div>
                         <div className="site-foot-nav-right">
                             <Navigation data={site.navigation} navClass="site-foot-nav-item" />
